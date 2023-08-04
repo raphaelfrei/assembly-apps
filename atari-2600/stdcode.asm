94ceb69a47eb89407121e88078005302a35badbd
Raphael Frei <raphaelrodriguesfrei@gmail.com>
2023-08-04 16:35:29 -0300

    Add files via upload

--- atari-2600/stdcode.asm
+++ atari-2600/stdcode.asm
@@ -1,18 +1,22 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;                 Raphael Frei - 2023                       ;
-;               Designed for Atari 2600                     ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                 Raphael Frei - 2023                       ;;
+;;               Designed for Atari 2600                     ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-processor 6502  ; Define the target processor
+    processor 6502  ; Define the target processor
 
-    seg code        ; Segment
+    seg Code        ; Segment
     org $F000       ; Define code origin in $F000 (First ROM space)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;              Fill the ROM size to exactly 4KB              ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+START:
+
+    jmp START
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;              Fill the ROM size to exactly 4KB              ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; The cartridge (and every 6502 code) requires this at the last 4 bytes
 
     org $FFFC       ; The bottom of the ROM
-    .word Start     ; Reset vector at $FFFC (Where the program starts)
-    .word Start     ; Interrupt vector at $FFFE (Unused in VCS)
+    .word START     ; Reset vector at $FFFC (Where the program starts)
+    .word START     ; Interrupt vector at $FFFE (Unused in VCS)
